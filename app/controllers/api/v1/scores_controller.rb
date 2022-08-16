f6def7fca1dad63d618511156e08f585c9962ddb
Jason Watkins <goushoryuken@gmail.com>
2022-08-16 15:16:38 -0700

    scores controller

---
+++ app/controllers/api/v1/scores_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::ScoresController < ApplicationController
+  before_action :set_score, only: [:show]
+
+  def index
+    @scores = Score.all 
+    render json: @scores
+  end
+
+  def show 
+    if @score
+      render json: @score
+    else
+      render json: @score.errors
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+  def set_score
+    @score = Score.find(params[:id])
+  end
+
+end
+end
