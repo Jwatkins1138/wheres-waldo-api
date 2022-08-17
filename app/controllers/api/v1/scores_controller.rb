1878ad31a5222a7ffcf2a22fef77c9b44c52b5a5
Jason Watkins <goushoryuken@gmail.com>
2022-08-17 10:02:06 -0700

    score create

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -15,16 +15,26 @@ class Api::V1::ScoresController < ApplicationController
   end
 
   def new
-
+    @score = Score.new
   end
 
   def create
+    @score = Score.new(score_params)
 
+    if @score.save
+      render json: @score
+    else
+      render json: @score.errors
+    end
   end
 
   private
   def set_score
     @score = Score.find(params[:id])
+  end
+
+  def score_parms
+    params.permit(:player, :seconds, :level_id)
   end
 
 end
